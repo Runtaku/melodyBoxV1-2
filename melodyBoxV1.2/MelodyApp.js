356ee310fed81b0875ad16b9d85510bae5b8aea1
runtaku_929 <daitaku929@gmail.com>
2025-06-21 14:48:27 +0900

    Update MelodyApp.js

--- melodyBoxV1.2/MelodyApp.js
+++ melodyBoxV1.2/MelodyApp.js
@@ -42,7 +42,7 @@ class MelodyApp {
 
 
         this.bindUI();
-        this.Compile(this.storage.melody == undefind ? this.cord :this.storage.melody);
+        this.Compile(this.storage === null ? this.cord :this.storage.melody);
         this.renderBox();
         this.export();
 
